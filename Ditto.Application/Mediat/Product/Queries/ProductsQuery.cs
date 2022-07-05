de91a574c07cd1bb752d945ec446607d2b42b010
Caonabo Mena <cmena7512@gmail.com>
2022-07-05 18:18:01 -0400

    feat: add default page

--- Ditto.Application/Mediat/Product/Queries/ProductsQuery.cs
+++ Ditto.Application/Mediat/Product/Queries/ProductsQuery.cs
@@ -8,6 +8,6 @@ public class ProductsQuery : IRequest<PaginationModel<ProductModel>>
 {
     public string Filter { get; set; }
     public int CategoryId { get; set; }
-    public int Page { get; set; }
+    public int Page { get; set; } = 1;
     public int Limit { get; set; } = 10;
 }

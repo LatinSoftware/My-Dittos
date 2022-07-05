00aadd3827eb7249cb020ad33ab910dc00dff2c7
Caonabo Mena <cmena7512@gmail.com>
2022-07-05 15:04:29 -0400

    refactor: change limit name

--- Ditto.Common/Repositories/IBaseRepository.cs
+++ Ditto.Common/Repositories/IBaseRepository.cs
@@ -6,7 +6,7 @@ namespace Ditto.Common.Repositories;
 public interface IBaseRepository<TEntity> where TEntity : class
 {
     public Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
-    Task<PaginationModel<TEntity>> GetAsync(int skip, int limit = 10, Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+    Task<PaginationModel<TEntity>> GetAsync(int skip, int take = 10, Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
     Task<TEntity> GetByIDAsync(object id);
     Task InsertAsync(TEntity entity);
     public void Update(TEntity entityToUpdate);

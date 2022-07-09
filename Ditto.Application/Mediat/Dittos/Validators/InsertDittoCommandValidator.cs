b6b9e59ea08403bcda7b41c11a249c766ec3cd10
Caonabo Mena <cmena7512@gmail.com>
2022-07-09 16:12:25 -0400

    feat: create ditto validations

---
+++ Ditto.Application/Mediat/Dittos/Validators/InsertDittoCommandValidator.cs
@@ -0,0 +1,18 @@
+using Ditto.Application.Mediat.Dittos.Commands;
+using Ditto.Application.Mediat.Product.Validators;
+using Ditto.Common.Repositories;
+using FluentValidation;
+
+namespace Ditto.Application.Mediat.Dittos.Validators;
+
+public class InsertDittoCommandValidator : AbstractValidator<InsertDittoCommand>
+{
+    public InsertDittoCommandValidator(IUnitOfWork repositories)
+    {
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.FrecuencyTime).NotEmpty();
+        RuleFor(x => x.Frecuency).NotEmpty();
+        RuleFor(x => x.OrderDate).NotEmpty().GreaterThanOrEqualTo(DateTime.Today);
+        RuleFor(x => x.ExpectedPrice).NotEmpty().GreaterThan(0);
+   }
+}
